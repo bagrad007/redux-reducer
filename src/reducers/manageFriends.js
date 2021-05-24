9544460a0e7fb557c4965f52be7c4643b7008608
Luke Gill <bagrad007@gmail.com>
2021-05-24 11:26:12 -0400

    commit tree 93e359e247f7b1daf36517379e2137d047424ae0

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+    friends: []
+}, action) {
+
+    if (action.type === "ADD_FRIEND") {
+        return { ...state, friends: [...state.friends, action.friend] }
+    } else if (action.type === "REMOVE_FRIEND") {
+        const foundIndex = state.friends.findIndex(friend => friend.id === action.id)
+        return (
+            {
+                ...state, friends: [...state.friends.slice(0, foundIndex),
+                ...state.friends.slice(foundIndex + 1)
+                ]
+            }
+        )
+    } else
+        return state
 }
